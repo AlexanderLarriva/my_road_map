940475c386537585e202858b4f72b29c83debf54
Alexander Shangin <alarriva@yandex.ru>
2023-02-10 13:13:44 +0600

    Add file

---
+++ decor_memo.py
@@ -0,0 +1,23 @@
+def memoized(func):
+  '''Function - decorator
+  
+  Demonstrates how closures and the decorator function work.'''
+  d = {}
+
+  def inner(arg):
+    if arg in d:
+      return d[arg]
+    else:
+      d[arg] = func(arg)
+    return d[arg]
+  return inner
+
+
+@memoized
+def f(x):
+    print('Calculating...')
+    return x * 10
+ 
+print(f(1))
+print(f(2))
+print(f(1))
